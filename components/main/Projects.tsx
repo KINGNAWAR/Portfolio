733d8ded844493ac383d83df2303987785311fd0
unknown <naouarm769@gmail.com>
2024-12-03 19:53:37 +0100

    9th commit

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -1,5 +1,3 @@
-
-
 import React from "react";
 import ProjectCard from "../sub/ProjectCard";
 
--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -5,7 +5,9 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 // @ts-ignore
 import * as random from "maath/random/dist/maath-random.esm";
+import dynamic from "next/dynamic";
 
+// StarBackground Component
 const StarBackground = (props: any) => {
   const ref: any = useRef();
   const [sphere] = useState(() => {
@@ -16,38 +18,38 @@ const StarBackground = (props: any) => {
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta/10;
     ref.current.rotation.y -= delta/15;
-  })
-
+  });
 
   return (
     <group rotation={[0,0, Math.PI / 4]}>
-        <Points
+      <Points
         ref={ref}
         positions={sphere}
         stride={3}
         frustumCulled
         {...props}
-        >
-            <PointMaterial
-                transparent
-                color="$fff"
-                size={0.002}
-                sizeAttenuation={true}
-                depthWrite={false}
-            />
-        </Points>
+      >
+        <PointMaterial
+          transparent
+          color="$fff"
+          size={0.002}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
     </group>
-  )
+  );
 };
 
-const StarsCanvas = () => (
-    <div className="w-full h-auto fixed inset-0 z-[-20]">
-        <Canvas camera={{position: [0, 0, 1]}}>
-        <Suspense fallback={null}>
-            <StarBackground />
-        </Suspense>
-        </Canvas>
-    </div>
-)
+// Use dynamic import with ssr: false to disable SSR for this component
+const StarsCanvas = dynamic(() => Promise.resolve(() => (
+  <div className="w-full h-auto fixed inset-0 z-[-20]">
+    <Canvas camera={{position: [0, 0, 1]}}>
+      <Suspense fallback={null}>
+        <StarBackground />
+      </Suspense>
+    </Canvas>
+  </div>
+)), { ssr: false });
 
 export default StarsCanvas;
