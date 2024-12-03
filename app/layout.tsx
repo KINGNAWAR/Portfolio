9958b4336c3fa241fce7242383dfac2e5bb19f91
unknown <naouarm769@gmail.com>
2024-12-03 19:21:21 +0100

    8th commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,25 @@
-import type { Metadata } from "next";
+import Head from "next/head";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import StarsCanvas from "@/components/main/StarBackground";
-import MouseParticleTrail from "@/components/sub/MouseParticleTrail"
+import MouseParticleTrail from "@/components/sub/MouseParticleTrail";
 import Navbar from "@/components/main/Navbar";
 import Footer from "@/components/main/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Space Portfolio",
-  description: "This is my portfolio",
-};
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.className} bg-[#000000] overflow-y-scroll `}
-      >
+      <body className={`${inter.className} bg-[#000000] overflow-y-scroll`}>
+        <Head>
+          <title>Space Portfolio</title>
+          <meta name="description" content="This is my portfolio" />
+        </Head>
         <MouseParticleTrail>
-        <StarsCanvas/>
-        <Navbar />
-        {children}
+          <StarsCanvas />
+          <Navbar />
+          {children}
         </MouseParticleTrail>
       </body>
     </html>
